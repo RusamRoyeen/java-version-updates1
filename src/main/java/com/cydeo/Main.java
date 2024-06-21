8a2eb1dbb96efd616774ab6429cb45757485d5c3
Rustam Royeen <rustam.royeen@gmail.com>
2024-06-21 18:41:50 -0400

    Generic is added to the main class

--- src/main/java/com/cydeo/Main.java
+++ src/main/java/com/cydeo/Main.java
@@ -6,11 +6,15 @@ import java.util.Objects;
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList items = new ArrayList();
+        ArrayList<Integer> items = new ArrayList<Integer>();
+
         items.add(1);
         items.add(2);
         items.add(3);
         items.add(4);
+        items.add("Apple");
+
+
 
         printDoubled(items);
 
