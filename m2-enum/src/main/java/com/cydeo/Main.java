596cfb37e5aa13f5f4d6ab4abb49c12828b49d21
Rustam Royeen <rustam.royeen@gmail.com>
2024-06-21 22:55:14 -0400

    Main.java is update with calculate method

--- m2-enum/src/main/java/com/cydeo/Main.java
+++ m2-enum/src/main/java/com/cydeo/Main.java
@@ -1,5 +1,7 @@
 package com.cydeo;
 
+import static com.cydeo.Operation.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,11 +14,45 @@ public class Main {
 
         System.out.println(Currency.PENNY);
         System.out.println("How to get all constant");
-        Currency[] currencies  = Currency.values();
+        Currency[] currencies = Currency.values();
         for (Currency currency : currencies) {
             System.out.println(currency);
 
         }
 
+        System.out.println("How to ue switch-case with enums");
+        switch (Currency.PENNY) {
+            case PENNY:
+                System.out.println("It is 1 cent");
+                break;
+            case NICKLE:
+                System.out.println("it is 5 cent");
+                break;
+            case DIME:
+                System.out.println("it is 10 cent");
+                break;
+            case QUARTER:
+                System.out.println("It is 25 cent");
+                break;
+        }
     }
+
+    private static void calculate(double x, double y, Operation operation) {
+        switch (operation) {
+            case PLUS:
+                System.out.println(x + y);
+                break;
+            case MINUS:
+                System.out.println(x - y);
+                break;
+            case MULTIPLY:
+                System.out.println(x * y);
+                break;
+            case DIVIDE:
+                System.out.println(x / y);
+                break;
+        }
+    }
+
 }
+
