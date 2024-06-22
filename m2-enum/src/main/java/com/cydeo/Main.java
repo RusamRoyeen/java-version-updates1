71f76781c458249ce040f945d422e017c92db2e6
Rustam Royeen <rustam.royeen@gmail.com>
2024-06-21 23:18:25 -0400

    Main.java is updated

--- m2-enum/src/main/java/com/cydeo/Main.java
+++ m2-enum/src/main/java/com/cydeo/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Apple a = new Apple(5);
+
         System.out.println("How to access a constant");
 
         Currency c = Currency.DIME;
