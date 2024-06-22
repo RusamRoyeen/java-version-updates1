20f138a54ab05afd999084bfa490fe5f0eeec70b
Rustam Royeen <rustam.royeen@gmail.com>
2024-06-21 22:29:20 -0400

    Main.java is updated.

---
+++ m2-enum/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,22 @@
+package com.cydeo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("How to access a constant");
+
+        Currency c = Currency.DIME;
+
+        System.out.println(c);
+
+        System.out.println(Currency.PENNY);
+        System.out.println("How to get all constant");
+        Currency[] currencies  = Currency.values();
+        for (Currency currency : currencies) {
+            System.out.println(currency);
+
+        }
+
+    }
+}
