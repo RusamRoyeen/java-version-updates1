7c72a3993afc2563ccfa3b07ad3eada3c5b7e10b
Rustam Royeen <rustam.royeen@gmail.com>
2024-06-24 18:26:51 -0400

    AppleTest.java is completed

--- m3-lambda/src/main/java/com/cydeo/AppleTest.java
+++ m3-lambda/src/main/java/com/cydeo/AppleTest.java
@@ -12,6 +12,25 @@ public class AppleTest {
         inventory.add(new Apple(100, Color.RED));
         inventory.add(new Apple(200, Color.GREEN));
         inventory.add(new Apple(50, Color.RED));
+
+        AppleHeavyPredicate abc = new AppleHeavyPredicate();
+        List<Apple> heavyApple = filterApples(inventory, abc);
+        System.out.println(heavyApple);
+
+        List<Apple> greenApple = filterApples(inventory, new AppleGreenColorPredicate());
+        System.out.println(greenApple);
+
+
     }
 
+    private static List<Apple> filterApples(List<Apple> inventory,ApplePredicate applePredicate) {
+
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (applePredicate.test(apple)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
 }
