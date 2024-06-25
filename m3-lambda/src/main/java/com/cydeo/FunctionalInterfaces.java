cd1aa3cd8592aed8901d8feb1ad92fca98ea0e7d
Rustam Royeen <rustam.royeen@gmail.com>
2024-06-24 22:45:17 -0400

    FunctionalInterfaces.java is updated

--- m3-lambda/src/main/java/com/cydeo/FunctionalInterfaces.java
+++ m3-lambda/src/main/java/com/cydeo/FunctionalInterfaces.java
@@ -1,5 +1,9 @@
 package com.cydeo;
 
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 public class FunctionalInterfaces {
     public static void main(String[] args) {
 
@@ -9,5 +13,24 @@ public class FunctionalInterfaces {
                 return apple.getWeight()>200;
             }
         };
+
+
+        System.out.println("******************* PREDICATE *************************");
+
+        Predicate<Integer> lesserThan = i -> i<18;
+        System.out.println(lesserThan.test(20));
+
+        System.out.println("******************* CONSUMER *************************");
+
+        Consumer<Integer> display = i -> System.out.println();
+        display.accept(10);
+
+        System.out.println("******************* BI CONSUMER *************************");
+
+        BiConsumer<Integer, Integer> addTwo = (x, y) ->System.out.println(x+y);
+        addTwo.accept(1,2);
+
+
+
     }
 }
