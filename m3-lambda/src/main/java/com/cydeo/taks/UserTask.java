dabcd7449da6a1f30f0473d98905c41076766bfa
Rustam Royeen <rustam.royeen@gmail.com>
2024-06-24 23:22:42 -0400

    UserTask.java is created.

---
+++ m3-lambda/src/main/java/com/cydeo/taks/UserTask.java
@@ -0,0 +1,31 @@
+package com.cydeo.taks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class UserTask {
+
+    public static void main(String[] args) {
+
+        List<User> users = new ArrayList<User>();
+        users.add(new User("Mike", "Smith", 35));
+        users.add(new User("Tom", "Evan", 25));
+        users.add(new User("Emma", "Pollard", 55));
+
+        //Print all elements in the list
+
+
+        //Print all people that last name starts with E
+
+
+    }
+    //Write Method
+    private static void printName(List<User> users, Predicate<User> p) {
+        for (User user : users) {
+            if(p.test(user)) {
+                System.out.println(user.toString());
+            }
+        }
+    }
+}
