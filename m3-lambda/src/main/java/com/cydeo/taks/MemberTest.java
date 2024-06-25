b4d2809c5096f21acadc3e10f11089553c2e2b13
Rustam Royeen <rustam.royeen@gmail.com>
2024-06-24 22:03:21 -0400

    MemberTest.java is created

---
+++ m3-lambda/src/main/java/com/cydeo/taks/MemberTest.java
@@ -0,0 +1,13 @@
+package com.cydeo.taks;
+
+public class MemberTest {
+
+    public static void main(String[] args) {
+
+        Person p1 = new Person("mike", 35, Gender.MALE);
+        Person p2 = new Person("ozzy", 25, Gender.MALE);
+        Person p3 = new Person("tom", 15, Gender.MALE);
+        Person p4 = new Person("alley", 45, Gender.FEMALE);
+
+    }
+}
