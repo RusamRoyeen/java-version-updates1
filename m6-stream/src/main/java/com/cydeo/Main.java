a0507d082ad83c2a70e7866dd68debd345cec3a6
Rustam Royeen <rustam.royeen@gmail.com>
2024-06-26 19:25:52 -0400

    m6-stream and Main.java are created

---
+++ m6-stream/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,10 @@
+package com.cydeo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Creating Stream from Array
+        String[] courses = {"Java", "Spring", "Agile"};
+
+    }
+}
