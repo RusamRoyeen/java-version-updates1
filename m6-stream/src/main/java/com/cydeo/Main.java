69874ee43f396bb13c757b3c1055f478360c261a
Rustam Royeen <rustam.royeen@gmail.com>
2024-06-26 19:37:50 -0400

    Main.java is updated for module 6

--- m6-stream/src/main/java/com/cydeo/Main.java
+++ m6-stream/src/main/java/com/cydeo/Main.java
@@ -16,6 +16,17 @@ public class Main {
         List<String> courseList= Arrays.asList("Java", "Spring", "Agile");
         Stream<String> courseStream2 = courseList.stream();
 
+        List<Course> myCourses = Arrays.asList(
+                new Course("Java", 100),
+                new Course("DS", 101),
+                new Course("MS", 102)
+        );
+
+        Stream<Course> myCourseStream = myCourses.stream();
+
+
+
+
 
 
     }
