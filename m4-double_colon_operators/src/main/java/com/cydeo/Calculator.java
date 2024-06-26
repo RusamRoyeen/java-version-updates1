b919ad8a740451a600233218be8365f3dd7b779b
Rustam Royeen <rustam.royeen@gmail.com>
2024-06-25 22:13:26 -0400

    Calculator.java is updated

--- m4-double_colon_operators/src/main/java/com/cydeo/Calculator.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/Calculator.java
@@ -3,10 +3,10 @@ package com.cydeo;
 public class Calculator {
 
     public static void findSum(int x, int y){
-        System.out.println("Sum" + (x=y));
+        System.out.println("Sum" + (x+y));
 
     }
-    public static void findMultiply(int x, int y){
-        System.out.println("Multiply" + (x*y));
+    public void findMultiply(int x, int y) {
+        System.out.println("Multiply" + (x * y));
     }
 }
