cc1530dd5a551b72295010c047ee1fa0bfb3054e
Rustam Royeen <rustam.royeen@gmail.com>
2024-06-26 18:51:33 -0400

    AppleTest.java is updated for module 5

--- m5-comparator_interface/src/main/java/com/cydeo/AppleTest.java
+++ m5-comparator_interface/src/main/java/com/cydeo/AppleTest.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+import static java.util.Comparator.comparing;
+
 public class AppleTest {
 
     public static void main(String[] args) {
@@ -14,9 +16,21 @@ public class AppleTest {
         inventory.add(new Apple(200, Color.GREEN));
         inventory.add(new Apple(50, Color.RED));
 
-        Comparator<Apple> sortApple = Comparator.comparing(Apple::getWeight);
+
+
+        Comparator<Apple> sortApple = comparing((Apple apple) -> apple.getWeight());
         inventory.sort(sortApple);
         System.out.println(inventory);
+
+        Comparator<Apple> sortApple2 = comparing((Apple apple) -> apple.getWeight());
+        inventory.sort(sortApple);
+        System.out.println(inventory);
+
+
+        inventory.sort(comparing(Apple::getWeight));
+        System.out.println(inventory);
+
+
 
     }
 }
