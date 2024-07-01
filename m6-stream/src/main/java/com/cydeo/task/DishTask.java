1ab851e317a7cee9bd18ef3cefff189cecb3ec89
Rustam Royeen <rustam.royeen@gmail.com>
2024-06-30 22:25:15 -0400

    DishTask.java is updated

--- m6-stream/src/main/java/com/cydeo/task/DishTask.java
+++ m6-stream/src/main/java/com/cydeo/task/DishTask.java
@@ -1,5 +1,7 @@
 package com.cydeo.task;
 
+import java.util.Comparator;
+
 public class DishTask {
 
     public static void main(String[] args) {
@@ -22,8 +24,20 @@ public class DishTask {
 
 
         //Print three high caloric dish name (>300)
+        DishData.getAll().stream()
+                .filter(dish -> dish.getCalories() > 300)
+                .map(Dish::getName)
+                .limit(3)
+                .forEach(System.out::println);
+
 
         //Print all dish name that are below 400 calories is sorted
+        DishData.getAll().stream()
+                .filter(dish -> dish.getCalories() < 400)
+                .sorted(Comparator.comparing(Dish::getCalories))
+                .map(Dish::getCalories)
+                .forEach(System.out::println);
+
 
     }
 }
