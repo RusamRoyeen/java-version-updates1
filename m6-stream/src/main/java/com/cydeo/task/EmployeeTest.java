8fdffe4feae6801053d27572b64496245e12efa9
Rustam Royeen <rustam.royeen@gmail.com>
2024-07-14 18:57:05 -0400

    EmployeeTest.java is updated.

--- m6-stream/src/main/java/com/cydeo/task/EmployeeTest.java
+++ m6-stream/src/main/java/com/cydeo/task/EmployeeTest.java
@@ -1,8 +1,32 @@
 package com.cydeo.task;
 
+import java.util.List;
+import java.util.stream.Stream;
+
+
+
 public class EmployeeTest {
 
     public static void main(String[] args) {
+
+        System.out.println("Print all email");
+        EmployeeData.readAll()
+                // .map(employee -> employee.getEmployeeEmailAddress())
+                .map(Employee::getEmployeeEmailAddress)
+                .forEach(System.out::println);
+
+        System.out.println("Print all Phone Numbers");
+        EmployeeData.readAll()
+                .flatMap(employee -> employee.getEmployeePhoneNumber().stream())
+                .forEach(System.out::println);
+
+
+        System.out.println("Print all Phone Numbers with Double Colon");
+        EmployeeData.readAll()
+                .map(Employee::getEmployeePhoneNumber)
+                .flatMap(List::stream)
+                .forEach(System.out::println);
+
 
 
     }
