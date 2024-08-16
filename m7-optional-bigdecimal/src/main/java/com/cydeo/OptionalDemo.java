44fb88f98a113478451109c18ed245b787cb8db0
Rustam Royeen <rustam.royeen@gmail.com>
2024-08-16 17:32:36 -0400

    m7-Optional-bigDecimal module is created

---
+++ m7-optional-bigdecimal/src/main/java/com/cydeo/OptionalDemo.java
@@ -0,0 +1,32 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalDemo {
+
+    public static void main(String[] args) {
+
+        List<Integer> number = Arrays.asList(2,4,2,10,23);
+
+        //empty() - isPresent()
+
+        Optional<String> empty = Optional.empty();
+        System.out.println(empty);
+//        System.out.println(empty.isPresent());
+//
+//
+//        Optional.of(number).isPresent();
+//        System.out.println(Optional.of(number).isPresent());
+
+//        number.stream().filter(x-> x>100).forEach(System.out::println);
+        System.out.println(empty.get());
+        //ifPresent
+        Optional<Integer> bigNumber = number.stream().filter(x -> x>100).findAny();
+
+
+
+
+    }
+}
