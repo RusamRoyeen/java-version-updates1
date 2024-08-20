c3a13568589e90ab7466cd952c3ba780bf8ab1ff
Rustam Royeen <rustam.royeen@gmail.com>
2024-08-20 17:31:26 -0400

    LocalVariableTypeDemo.java is updated

--- m8-new-version-updates/src/main/java/com/cydeo/java10/LocalVariableTypeDemo.java
+++ m8-new-version-updates/src/main/java/com/cydeo/java10/LocalVariableTypeDemo.java
@@ -1,10 +1,27 @@
 package com.cydeo.java10;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LocalVariableTypeDemo {
 
     public static void main(String[] args) {
 
        //Type Inference
+        var str = "Cydeo";
+        var number = 5;
+
+        byte x=1;
+        var y=1;
+        var price = 12.50;
+
+       var numbers = Arrays.asList(3,4,5,6);
+       var sum = 0;
+       for(var eachNumber:numbers){
+           sum += eachNumber;
+       }
+
+
 
     }
 }
